cd1190b1d3ae93f51e124fb56e15ef124f95c314
Vitamarys <vitaliy.marysiy@gmail.com>
2020-12-08 17:06:41 +0200

    add open btn om mobile

--- open.js
+++ open.js
@@ -1,15 +1,29 @@
 const subTitle = $('.tabs__block__item__subtitle');
-
-$('.tabs__block__item__open').click(function(e){
+const openBtn = $('.tabs__block__item__open');
+const descr = $('.tabs__block__item__desc');
+const file = $('.tabs__block__item__file');
+const item = $('.tabs__block__item');
+const addFile = $('.tabs__block__item__add');
+item.click(function(){
    
-//    $('.tabs__block__item__open').removeClass('active');
-//    $(this).addClass('active');
-//     console.log($('.tabs__block__item__open').length)
-//      if( $(this).hasClass('active')){
-//         $(this).find($('.tabs__block__item__subtitle'));
-//          console.log($(this));
-//          $('.tabs__block__item__subtitle').addClass('active');
-//          console.log(subTitle.length);
-//      }
+    item.removeClass('active');
+   $(this).addClass('active');
+    if ( $(this).hasClass('active')){
+        $(this).find(subTitle).addClass('active');
+        $(this).find(descr).addClass('active');
+        $(this).find(file).addClass('active');
+        $(this).find( addFile).addClass('active');
+        $(this).find(openBtn).addClass('active');
+        
+    }
+   
+})
+openBtn.click(function(){
+    subTitle.removeClass('active');
+    descr.removeClass('active');
+    file.removeClass('active');
+     addFile.removeClass('active');
+    openBtn.removeClass('active');
+    
     
 })
