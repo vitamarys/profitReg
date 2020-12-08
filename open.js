b1bfc8a56a3c24fc2ad41322ab4a58340be8f320
Vitamarys <vitaliy.marysiy@gmail.com>
2020-12-08 16:17:00 +0200

    add adaptetion

---
+++ open.js
@@ -0,0 +1,15 @@
+const subTitle = $('.tabs__block__item__subtitle');
+
+$('.tabs__block__item__open').click(function(e){
+   
+//    $('.tabs__block__item__open').removeClass('active');
+//    $(this).addClass('active');
+//     console.log($('.tabs__block__item__open').length)
+//      if( $(this).hasClass('active')){
+//         $(this).find($('.tabs__block__item__subtitle'));
+//          console.log($(this));
+//          $('.tabs__block__item__subtitle').addClass('active');
+//          console.log(subTitle.length);
+//      }
+    
+})
